a44a0b39297335fb87963baece6867c7cb6d0735
hedi <hedinoureddine4@gmail.com>
2024-06-17 00:40:55 +0200

    first commit

---
+++ data.js
@@ -0,0 +1,25 @@
+let shopitemsdata=[
+    {
+        id: "first",
+        name:"balsa_carbo",
+        price:"60.90eur",
+        img:"balsa_carbo_x7.1_2.jpg"
+    },
+    {
+        id: "second",
+        name:"blade xstarv fl 05",
+        price:"80.90eur",
+        img:"blade_xstarv_fl_05.jpg",
+    },
+    {
+        id: "third",
+        name:"viscaria",
+        price:"160.90eur",
+        img:"tt.php5.jpg",
+    },
+    {
+        id: "forth",
+        name:"hadraw",
+        price:"70.90eur",
+        img:"hadraw_3_2.jpg",
+    },]
